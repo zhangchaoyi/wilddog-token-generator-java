b8310ae76bbc8c1cb4464ac096c30959be82fd18
zhangchaoyi <zhangchaoyi@wilddog.com>
2016-01-07 11:18:36 +0800

    sec changed

--- wilddog-token-generator-java/src/main/java/com/wilddog/security/token/JWTEncoder.java
+++ wilddog-token-generator-java/src/main/java/com/wilddog/security/token/JWTEncoder.java
@@ -20,6 +20,7 @@ public class JWTEncoder {
     private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
     private static final String HMAC_256 = "HmacSHA256";
     String s ="";
+    String s1;
     /**
      * Encode and sign a set of claims.
      *
